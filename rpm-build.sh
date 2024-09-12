ada83ee1fb282a71b6bac6f30454f7406db500e9
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2024-09-12 18:27:39 +0200

    Using mock

--- rpm-build.sh
+++ rpm-build.sh
@@ -13,30 +13,9 @@ mkdir -p ${TOPDIR}/SOURCES
 cp -f $SPECFILE ${TOPDIR}/SPECS
 spectool -C ${TOPDIR}/SOURCES  -g ${TOPDIR}/SPECS/$SPECFILE
 
-rpmbuild --define "_topdir $TOPDIR" -ba ${TOPDIR}/SPECS/$SPECFILE
-
-exit 0
-
-
-rm -rf build
-# def
-export TOPDIR=$(pwd)/build/rpmbuild
-#rpmdev-setuptree --define "_rpmdir $TOPDIR"
-
-# make TOPDIR
-mkdir -p ${TOPDIR}
-
-# copy SPECS to TOPDIR
-cp -rf SPECS ${TOPDIR}
-# copy SOURCES to TOPDIR
-cp -rf SOURCES ${TOPDIR}
-
-# build
-spectool -C ${TOPDIR}/SOURCES -g ${TOPDIR}/SPECS/kafka.spec
-
 # Direct
-#rpmbuild --define "_topdir $TOPDIR" -ba ${TOPDIR}/SPECS/kafka.spec
+# rpmbuild --define "_topdir $TOPDIR" -ba ${TOPDIR}/SPECS/$SPECFILE
 
 # mock
-rpmbuild -bs --nodeps --define "_topdir $TOPDIR" ${TOPDIR}/SPECS/kafka.spec
-mock --enable-network $TOPDIR/SRPMS/kafka*.src.rpm
+rpmbuild -bs --nodeps --define "_topdir $TOPDIR" ${TOPDIR}/SPECS/$SPECFILE
+mock --enable-network $TOPDIR/SRPMS/opensearch-connector-for-apache-kafka*.src.rpm
